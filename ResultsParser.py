e7b7773dca4a42fd603c3a2ff2d3a03084729d05
Gabriele Dragotto <6563103+gdragotto@users.noreply.github.com>
2021-01-29 09:53:27 -0500

    Instances Insights and new results parser

--- ResultsParser.py
+++ ResultsParser.py
@@ -6,11 +6,15 @@ if __name__ == '__main__':
     with open('sumup.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["InstanceName", "Tax", "Trade",
-                         "One_Production", "One_DomesticPrice", "One_F1_Type", "One_Production_F1", "One_F2_Type",
-                         "One_Production_F2", "One_F3_Type", "One_Production_F3", "One_Import", "One_Export",
+                         "One_Production", "One_DomesticPrice", "One_F1_Type", "One_F1_Emission", "One_Production_F1",
+                         "One_F2_Type",
+                         "One_F2_Emission", "One_Production_F2", "One_F3_Type", "One_F3_Emission ", "One_Production_F3",
+                         "One_Import", "One_Export",
                          "One_ExportPrice", "One_Tax",
-                         "Two_Production", "Two_DomesticPrice", "Two_F1_Type", "Two_Production_F1", "Two_F2_Type",
-                         "Two_Production_F2", "Two_F3_Type", "Two_Production_F3", "Two_Import", "Two_Export",
+                         "Two_Production", "Two_DomesticPrice", "Two_F1_Type", "Two_F1_Emission", "Two_Production_F1",
+                         "Two_F2_Type",
+                         "Two_F2_Emission", "Two_Production_F2", "Two_F3_Type", "Two_F3_Emission", "Two_Production_F3",
+                         "Two_Import", "Two_Export",
                          "Two_ExportPrice", "Two_Tax"])
         for i in range(0, 50):
             for tax in [0, 1]:
@@ -32,13 +36,16 @@ if __name__ == '__main__':
                                      ProdOne,
                                      insData['Countries'][0]['DemandParam']['Alpha'] -
                                      insData['Countries'][0]['DemandParam']['Beta'] * (
-                                                 solutionData[Countries[0]['NetImport']] - solutionData[
-                                             Countries[0]['NetExport']] + ProdOne),
+                                             solutionData[Countries[0]['NetImport']] - solutionData[
+                                         Countries[0]['NetExport']] + ProdOne),
                                      insData['Countries'][0]['Followers']['Names'][0],
+                                     insData['Countries'][0]['Followers']['EmissionCosts'][0],
                                      solutionData[Countries[0]['FollowerStart']],
                                      insData['Countries'][0]['Followers']['Names'][1],
+                                     insData['Countries'][0]['Followers']['EmissionCosts'][1],
                                      solutionData[Countries[0]['FollowerStart'] + 1],
                                      insData['Countries'][0]['Followers']['Names'][2],
+                                     insData['Countries'][0]['Followers']['EmissionCosts'][2],
                                      solutionData[Countries[0]['FollowerStart'] + 2],
                                      solutionData[Countries[0]['NetImport']], solutionData[Countries[0]['NetExport']],
                                      solutionData[Countries[0]['ShadowPrice']],
@@ -49,13 +56,18 @@ if __name__ == '__main__':
                                              solutionData[Countries[1]['NetImport']] - solutionData[
                                          Countries[1]['NetExport']] + ProdTwo),
                                      insData['Countries'][1]['Followers']['Names'][0],
+                                     insData['Countries'][1]['Followers']['EmissionCosts'][0],
                                      solutionData[Countries[1]['FollowerStart']],
                                      insData['Countries'][1]['Followers']['Names'][1],
+                                     insData['Countries'][1]['Followers']['EmissionCosts'][1],
                                      solutionData[Countries[1]['FollowerStart'] + 1],
                                      insData['Countries'][1]['Followers']['Names'][2],
+                                     insData['Countries'][1]['Followers']['EmissionCosts'][2],
                                      solutionData[Countries[1]['FollowerStart'] + 2],
                                      solutionData[Countries[1]['NetImport']], solutionData[Countries[1]['NetExport']],
                                      solutionData[Countries[1]['ShadowPrice']],
                                      solutionData[Countries[1]['Tax']]])
                     res.close()
                     ins.close()
+
+   
